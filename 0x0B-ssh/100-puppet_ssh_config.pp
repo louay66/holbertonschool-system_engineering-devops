5ac27afe757ab71ce4a439d8b4a3e1b2af51135c
louay66 <louaylouay20@gmail.com>
2022-08-14 17:57:21 +0100

    conf via puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#changes to our configuration file
+file_line {'ssh_config':
+ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => "PasswordAuthentication no",
+  match  => 'PasswordAuthentication yes',
+}
+file_line {'ssh config':
+ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => "IdentityFile ~/.ssh/school",
+  match  => 'IdentityFile ~/.ssh/school',
+}

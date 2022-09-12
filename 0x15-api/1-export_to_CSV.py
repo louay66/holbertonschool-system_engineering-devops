13654559ee2a7bb85cacc0707f17afccd70e374d
louay66 <louaylouay20@gmail.com>
2022-09-12 21:58:52 +0100

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """exprt data in csv format"""
 import csv
-import requests
+from requests import get
 from sys import argv
 
 
 if __name__ == "__main__":
-
     user_id = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(user_id))
-    name_user = user.json().get('username')
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
 
-    filename = user_id + '.csv'
-    with open(filename, mode='w') as a:
-        write = csv.writer(a, delimiter=',', quotechar='"',
-                           quoting=csv.QUOTE_ALL)
-        for line in todo.json():
-            if line.get('userId') == int(user_id):
-                write.writerow([user_id, name_user, str(line.get('completed')),
-                                line.get('title')])
+    url = "https://jsonplaceholder.typicode.com"
+
+    user = get("{}/users/{}".format(url, user_id)).json()
+
+    todo = get("{}/users/{}/todos".format(url, user_id)).json()
+
+    user_name = user.get('username')
+
+    file = '{}.csv'.format(user_id)
+
+    with open(file, 'w') as a:
+
+        for line in todo:
+            line = [line['userId'], user_name,
+                    line['completed'], line['title']]
+            write = csv.writer(a, quoting=csv.QUOTE_ALL)
+            write.writerow(line)

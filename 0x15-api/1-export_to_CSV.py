c5ac4e7c0a26b8ae018c9abee2910d4c4f984998
louay66 <louaylouay20@gmail.com>
2022-09-12 21:13:23 +0100

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""fetch api and put it with csv"""
+"""exprt data in csv format """
 import csv
 import requests
 from sys import argv
@@ -7,17 +7,17 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    user_id = argv[1]
+    userId = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(user_id))
-    name_user = user.json().get('username')
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    filename = user_id + '.csv'
-    with open(filename, mode='w') as a:
-        write = csv.writer(a, delimiter=',', quotechar='"',
-                           quoting=csv.QUOTE_ALL)
-        for line in todo.json():
-            if line.get('userId') == int(user_id):
-                write.writerow([user_id, name_user, str(line.get('completed')),
-                                line.get('title')])
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])

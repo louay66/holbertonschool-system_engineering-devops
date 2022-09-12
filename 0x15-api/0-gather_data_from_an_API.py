9448d88dd3425c6d109441d2f161a0654ef7037e
louay66 <louaylouay20@gmail.com>
2022-09-12 17:36:48 +0100

    first fetch api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" fetch api"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    user_id = argv[1]
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    name_user = user.json().get('name')
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    avrge_todos = 0
+    completed = 0
+
+    for tk in todo.json():
+        if tk.get("userId") == int(user_id):
+            avrge_todos += 1
+            if tk.get('completed'):
+                completed += 1
+    print(
+        f"Employee {name_user} is done with tasks({completed}/{avrge_todos})")
+
+    for titel in todo.json():
+        if titel.get("userId") == int(user_id):
+            if titel.get('completed'):
+                print(titel.get('title'))

d34518bcd8a5280ffe7c5b3d3137d2922f44220e
louay66 <louaylouay20@gmail.com>
2022-09-12 22:38:16 +0100

    json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """export data in json format"""
 
-import json
-import requests
+from json import dump
+from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
-    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(argv[1])).json()
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user = get("{}/users/{}".format(url, user_id)).json()
+    todo = get('{}/todos?userId={}'.format(url, user_id)).json()
+    u_name = user.get('username')
+    format = {}
+    format[user_id] = []
+    file = '{}.json'.format(user_id)
+    for value in todo:
+        append = {'task': value['title'], 'completed': value['completed'],
+                  'username': u_name}
+        format[user_id].append(append)
 
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1])).json()
-
-    user_dict = {str(user.get('id')): []}
-    for todo in todos:
-        td = {"task": todo.get('title'),
-              "completed": todo.get('completed'),
-              "username": user.get('username')}
-        user_dict[str(user.get('id'))].append(td)
-
-    with open(str(user.get('id'))+".json", 'w') as f:
-        json.dump(user_dict, f)
+    with open(file, 'w') as a:
+        dump(format, a)

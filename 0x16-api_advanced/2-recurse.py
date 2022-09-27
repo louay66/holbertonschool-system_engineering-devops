c7d148e00269373b01ad4f01917f5fc7656846d7
louay66 <louaylouay20@gmail.com>
2022-09-27 15:55:16 +0100

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" api advanced"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """
+
+    :param subreddit:
+    :param hot_list:
+    :param after:
+    :return:
+    """
+
+    data = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                        "/hot?after={}"
+                        .format(subreddit, after),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if data.status_code != 200:
+        return None
+    else:
+        dict_data = data.json()
+        if len(dict_data.get("data").get("children")) == 0:
+            return hot_list
+        else:
+            for child in dict_data.get("data").get("children"):
+                hot_list.append(child.get("data").get("title"))
+            after = dict_data.get("data").get("after")
+            if after is None:
+                return hot_list
+            return recurse(subreddit, hot_list, after=after)

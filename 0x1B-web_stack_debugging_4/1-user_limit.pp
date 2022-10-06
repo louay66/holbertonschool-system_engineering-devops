414650fb7abc9bd5ff4682b1849364a85bc7ebcc
louay66 <louaylouay20@gmail.com>
2022-10-06 14:15:55 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,11 @@
 # Change the OS configuration to login with the holberton
 
 exec { 'hard':
-  path => '/usr/local/bin/:/bin/',
   command => 'sed -i "/holberton hard/s/5/20000/" /etc/security/limits.conf',
-  provider => 'shell',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'soft':
-  path => '/usr/local/bin/:/bin/',
   command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
-  provider => 'shell',
+  path    => '/usr/local/bin/:/bin/'
 }
